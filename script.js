620312412d11aa49b319c459416b693f986bc4c8
Amandine <amandinejeanjules@free.fr>
2022-02-19 08:26:18 +0100

    Switch

--- script.js
+++ script.js
@@ -5,8 +5,7 @@ let globalScore1;
 let playerOn = "player1";
 let scoreText;
 let globalText;
-let score;
-let global;
+
 /*INITIALISATION*/
 let newGame = () => {
   document.getElementById("score1").textContent = 0;
@@ -17,7 +16,7 @@ let newGame = () => {
 document.getElementById("buttonNewGame").addEventListener("click", newGame);
 newGame();
 
-/*ROLL BUTTON/AFFICHAGE DE AND DICE IMAGE*/
+/*ROLL BUTTON/AFFICHAGE SCORE AND DICE IMAGE*/
 let Dice = () => {
   randomNumber = Math.floor(Math.random() * 6) + 1;
   document
@@ -31,44 +30,56 @@ let Dice = () => {
     switchPlayer();
   }
 };
+
 document.getElementById("rollTheDice").addEventListener("click", Dice);
 
 /*SWITCH PLAYER*/
 function switchPlayer() {
   if (playerOn === "player1") {
     playerOn = "player2";
-    alert("Passez votre tour! Place au player2!");
+    alert(" Place au player2!");
   } else {
     playerOn = "player1";
-    alert("Passez votre tour! Place au player1!");
+    alert(" Place au player1!");
+  }
+
+  switch (playerOn) {
+    case "player1": {
+      console.log("1");
+      scoreText = document.getElementById("score1");
+      globalText = document.getElementById("globalScore1");
+      score = score1;
+      global = globalScore1;
+      break;
+    }
+    case "player2": {
+      console.log("2");
+      scoreText = document.getElementById("score2");
+      globalText = document.getElementById("globalScore2");
+      score = score2;
+      global = globalScore2;
+      break;
+    }
+    default: {
+      console.log("erreur Player");
+    }
   }
 }
-
-switch (playerOn) {
-  case "player1": {
-    console.log("1");
-    scoreText = document.getElementById("score1");
-    globalText = document.getElementById("globalScore1");
-    score = score1;
-    global = globalScore1;
-    break;
-  }
-  case "player2": {
-    console.log("2");
-    scoreText = document.getElementById("score2");
-    globalText = document.getElementById("globalScore2");
-    score = score2;
-    global = globalScore2;
-    break;
-  }
-  default: {
-    console.log("erreur Player");
-  }
-}
-
+scoreText = document.getElementById("score1").textContent;
+globalText = document.getElementById("globalScore1").textContent;
+scoreText = document.getElementById("score2").textContent;
+globalText = document.getElementById("globalScore2").textContent;
+let score = document.getElementById("score1").textContent;
+let global = document.getElementById("globalScore1");
 /*HOLD BUTTON*/
 let Hold = () => {
-  document.getElementById("globalScore1").textContent +=
-    document.getElementById("score1").textContent;
+  {
+    score.textContent = randomNumber;
+    global.textContent += score;
+
+    console.log("alert");
+  }
+
+  switchPlayer();
 };
 document.getElementById("hold").addEventListener("click", Hold);
