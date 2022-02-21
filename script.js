de1d3723df108a28b52b5122fd01129305f6c973
Amandine <amandinejeanjules@free.fr>
2022-02-21 14:26:13 +0100

    Functions winner, current player

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ let scoreText1;
 let globalText1;
 let scoreText2;
 let globalText2;
-
+let border1 = document.getElementById("card1");
+let border2 = document.getElementById("card2");
 /*INITIALISATION*/
 let newGame = () => {
   document.getElementById("score1").textContent = 0;
@@ -35,33 +36,30 @@ let Dice = () => {
     switchPlayer();
   }
 };
-
 document.getElementById("rollTheDice").addEventListener("click", Dice);
-
 /*SWITCH PLAYER*/
-function switchPlayer() {
+let switchPlayer = () => {
   if (playerOn === "player1") {
     playerOn = "player2";
-    alert(" Place au player2!");
   } else {
     playerOn = "player1";
-    alert(" Place au player1!");
   }
-
   switch (playerOn) {
     case "player1": {
       console.log("1");
+      border1.setAttribute("style", "border: solid 15px #D885A3");
+      border2.setAttribute("style", "border: solid 15px #ffe4c0");
       scoreText1 = document.getElementById("score1");
       globalText1 = document.getElementById("globalScore1");
-
       document.getElementById("score2").textContent = 0;
       break;
     }
     case "player2": {
       console.log("2");
+      border2.setAttribute("style", "border: solid 15px #D885A3");
+      border1.setAttribute("style", "border: solid 15px #ffe4c0");
       scoreText2 = document.getElementById("score2");
       globalText2 = document.getElementById("globalScore2");
-
       global = globalScore2;
       document.getElementById("score1").textContent = 0;
       break;
@@ -70,13 +68,12 @@ function switchPlayer() {
       console.log("erreur Player");
     }
   }
-}
+};
 let global1;
 scoreText1 = document.getElementById("score1");
 globalText1 = document.getElementById("globalScore1").textContent;
 scoreText2 = document.getElementById("score2").textContent;
 globalText2 = document.getElementById("globalScore2").textContent;
-
 let global = document.getElementById("globalScore1");
 /*HOLD BUTTON*/
 let Hold = () => {
@@ -92,5 +89,15 @@ let Hold = () => {
   switchPlayer();
 };
 document.getElementById("hold").addEventListener("click", Hold);
-/*global += score;
-    globalText += global;*/
+
+let finish = document.getElementById("rollTheDice");
+let Winner = () => {
+  if (globalText1 >= 5) {
+    finish.setAttribute("disabled", "true");
+    alert("Le joueur 1 a gagné");
+  }
+  if (globalText2) {
+    finish.setAttribute("disabled", "true");
+    alert("Le joueur 2 a gagné");
+  }
+};
